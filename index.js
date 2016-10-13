407e244249fc2f7965b1b1e9d2615f2bd258dc6d
xxKobalxx <brandonmarella@gmail.com>
2016-10-13 03:02:58 +0000

    Moving back to request library

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
-const program = require('commander');
-const request = require('request');
-const fs      = require('fs');
+const program   = require('commander');
+const request   = require('request');
+const fs        = require('fs');
 
 program.parse(process.argv); // notice that we have to parse in a new statement.
+
+if (program.args.length === 0) {
+  program.help();
+  process.exit();
+}
 
 function sift() {
   if (program.args[0] == null) {
@@ -21,7 +26,7 @@ function sift() {
     privatePackage = true;
   }
 
-  var recipeRepo = [];
+  let recipeRepo = [];
   for (let i=privatePackage ? 1 : 0; i<inputSplit.length; i++) {
     recipeRepo.push(inputSplit[i]);
   }
@@ -33,31 +38,17 @@ function sift() {
   }
 }
 
+// https://raw.githubusercontent.com/MarellaB/tbd-gulp/master/gulpfile.js
 function getRepo() {
-  var recipeInfo = sift();
-  if (recipeInfo.private) {
-    console.log('Pulling private recipe ' + recipeInfo.name + ' by ' + recipeInfo.author);
-  }
-  return {
-    gulpfile: request('https://raw.githubusercontent.com/' + recipeInfo.author + '/'
-      + recipeInfo.name + '/master/gulpfile.js',
-      function(error, response, body) {
-        return body;
-    }),
-    config: request('https://raw.githubusercontent.com/' + recipeInfo.author + '/'
-      + recipeInfo.name + '/master/.tbdconfig',
-      function(error, response, body) {
-        return body;
-    })
-  };
+  let recipeInfo = sift();
+  request('https://raw.githubusercontent.com/' + recipeInfo.author + '/' +  + '/master/gulpfile.js').pipe(fs.createWriteStream('doodle.png'))
+  
 }
 var recipeRepo = getRepo();
 fs.writeFile("gulpfile.js", recipeRepo.gulpfile, function(err) {
   if (err) {
     return console.log('err');
   }
-  console.log(recipeRepo.gulpfile.body);
+  console.log(recipeRepo.gulpfile);
   console.log('Gulpfile created');
-})
-
-if (program.args.length === 0) program.help();
+});
